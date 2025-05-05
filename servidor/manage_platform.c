da7ae195746b774b2541427c3c4e304653b2cb9c
100495839 <100495839@alumnos.uc3m.es>
2025-05-05 19:14:57 +0200

    - connect (parte servidor) FALTA VERIFICAR SI USUARIO REGISTRADO - is_file_in_directory() sirve para saber si un archivo está en cierto directorio, verificando primero si el directorio existe

--- servidor/manage_platform.c
+++ servidor/manage_platform.c
@@ -2,6 +2,43 @@
 #include <stdio.h>
 #include <unistd.h>
 
+// -1 : error
+// 0 : directorio existe, archivo no
+// 1 : directorio existe, archivo también
+// 2 : directorio no existe (archivo tampoco)
+int is_file_in_directory(char *dirname, char *filepath){
+    // Directorio "active_users" existe?
+    DIR* dir = opendir(dirname);
+    int result;
+    
+    // Directorio existe
+    if (dir) {
+        // Existe usuario?
+        if (access(filepath, F_OK) == 0){
+            // Archivo existe
+            result = 1;
+        }
+        else{
+            // Archivo no existe
+            result = 0;
+        }
+        // Cerrar directorio
+        if (closedir(dir) == -1){
+            result = -1;
+        }
+    } 
+    // Directorio no existe
+    else if (ENOENT == errno) {
+        result = 2;
+    } 
+    // Otro error abriendo directorio
+    else {
+        result = -1;
+    }
+
+    return result;
+}
+
 int register_user(char* user){
     // verificar antes si el archivo user existe
     if (access(user, F_OK) == 0){
@@ -38,10 +75,70 @@ int unregister_user(char* user){
     }
 }
 
+int connect_user(char* user, char* ip_user, int port_user){
+    // 0 en caso de exito
+    // 1 si el usuario no registrado
+    // 2 si el usuario ya está conectado
+    // 3 en cualquier otro caso.
+    
+    // ==========MODIFICAR===========
+    // (1) Usuario registrado?
+    int is_user_registered = 1;
+    
+    if (is_user_registered == 0){
+        return 1;
+    }
+    
+    // (2) Usuario ya conectado?
+
+    char *dirname_active = "active_users";
+    char filepath_active[300];
+    // Nombre del archivo del usuario (en el caso de existir)
+    if (sprintf(filepath_active, "%s/%s", dirname_active, user) < 0){
+        return 3;
+    }
+
+    int is_user_connected = is_file_in_directory(dirname_active, filepath_active);
+
+    // Error
+    if (is_user_connected == -1){
+        return 3;
+    }
+    // Usuario conectado
+    else if (is_user_connected == 1){
+        return 2;
+    }
+    // Directorio no existe, crearlo
+    else if (is_user_connected == 2){
+        if (mkdir(dirname_active, 0755) == -1){
+            return 3;
+        }
+    }
+    
+    // (3) Añadir archivo correspondiente al directorio
+
+    FILE *user_active_file  = fopen(filepath_active, "w+");
+    if (user_active_file == NULL){
+        return 3;
+    }
+    // Escribir IP y puerto
+    if (fprintf(user_active_file, "%s/%d", ip_user, port_user) < 0){
+        fclose(user_active_file);
+        return 3;
+    }
+    if (fclose(user_active_file)){
+        return 3;
+    }
+
+    return 0;
+}
+
 int main(int argc, char const *argv[])
 {
-    char *usuario = "user/Francisco";
-    int res = unregister_user(usuario);
+    // char *usuario = "user/Francisco";
+    // int res = unregister_user(usuario);
+
+    int res = connect_user("Francisco", "10.128.1.253", 5000);
 
     printf("Res: %d\n", res);
 
--- servidor/manage_platform.h
+++ servidor/manage_platform.h
@@ -1,10 +1,16 @@
 #ifndef MANAGE_PLATFORM_H
 #define MANAGE_PLATFORM_H
 
+#include <dirent.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+int is_file_in_directory(char *filepath, char *user);
+
 int register_user(char* user);
 int unregister_user(char* user);
 
-int connect_user(char* user, int port);
+int connect_user(char* user, char* ip_user, int port_user);
 int disconnect_user(char *user);
 int publish(char* user, char* file_path, char* description);
 int delete(char* user, char* file_path);
